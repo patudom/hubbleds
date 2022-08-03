9ed29be337fcb5871ec479b4787a7f9922053263
Nicholas Earl <contact@nicholasearl.me>
2022-08-03 14:30:58 -0500

    Refactor component imports

---
+++ src/cosmicds_hubble/components/spectrum_slideshow/spectrum_slideshow.py
@@ -0,0 +1,23 @@
+import ipyvuetify as v
+from cosmicds.utils import load_template
+from glue_jupyter.state_traitlets_helpers import GlueState
+from traitlets import Int, Bool
+
+
+# theme_colors()
+
+class SpectrumSlideshow(v.VuetifyTemplate):
+    template = load_template("spectrum_slideshow.vue", __file__,
+                             traitlet=True).tag(sync=True)
+    step = Int(0).tag(sync=True)
+    length = Int(11).tag(sync=True)
+    dialog = Bool(False).tag(sync=True)
+    # currentTitle = Unicode("").tag(sync=True)
+    state = GlueState().tag(sync=True)
+
+    # exploration_complete = Bool(False).tag(sync=True)
+
+    def __init__(self, story_state, *args, **kwargs):
+        self.state = story_state
+
+        super().__init__(*args, **kwargs)
