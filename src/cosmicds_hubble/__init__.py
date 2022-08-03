6819d3b51e114b46f123663e363ca68cb3134689
Nicholas Earl <contact@nicholasearl.me>
2022-08-03 14:33:39 -0500

    Move story-specific files to new split-repo package format

--- src/cosmicds_hubble/__init__.py
+++ src/cosmicds_hubble/__init__.py
@@ -1,16 +1,10 @@
-import sys
+from pathlib import Path
+from cosmicds import STORY_PATHS
+from .story import *
+from .stages import *
+from .tools import *
+from .viewers import *
+from .components import *
 
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "cosmicds-hubble"
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+STORY_PATHS['hubble'] = Path(__file__).parent / "Notebook.ipynb"
