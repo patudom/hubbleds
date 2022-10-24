990e93dcd62cb458d57cb1a73cf4c51f7096f1fd
Pat Udomprasert <pudompra@cfa.harvard.edu>
2022-10-23 23:56:02 -0400

    Avoid fractions in definitions of ' and " for now.
    We can discuss with teachers what they think is best, but my gut reaction is that students might be more confused by the fractions.

--- src/hubbleds/components/generic_state_components/stage_two/guideline_angsize_meas2b.vue
+++ src/hubbleds/components/generic_state_components/stage_two/guideline_angsize_meas2b.vue
@@ -10,10 +10,10 @@
       class="mb-4"
     >
     <p> 
-      The angular scale is measured in either in degrees (&#176;&thinsp;), arcminutes (&thinsp;'&thinsp;), or arcseconds (&thinsp;"&thinsp;) as you zoom in.  
+      The angular scale is measured in degrees (&#176;&thinsp;), arcminutes (&thinsp;'&thinsp;), or arcseconds (&thinsp;"&thinsp;) as you zoom in.  
     </p>
      <p> 
-      One <strong>arcminute</strong> is 1/60 of a degree, and one <strong>arcsecond</strong> is 1/60 of an arcminute 
+      There are 60 <strong>arcminutes</strong> in 1 degree, and 60 <strong>arcseconds</strong> in 1 arcminute.
     </p>
     <p> The data in your table will be measured in arcseconds.</p>
       
