dbd4eac4d2ad9d91906a5102a6b5850ef19fb4ae
John Arban Lewis <johnarban@users.noreply.github.com>
2022-10-26 19:17:19 -0400

    including forgotten file

---
+++ src/hubbleds/components/prodata_components/guideline_professional_data7.vue
@@ -0,0 +1,87 @@
+<!-- # multiple choice -->
+<template>
+  <v-alert
+    color="info"
+    class="mb-4 mx-auto"
+    max-width="800"
+    elevation="6"
+  >
+    <h3
+      class="mb-4"
+    >
+      Professional Data
+    </h3>
+    <div
+      class="mb-4"
+    >
+      Whose age estiamte are you more inclinded to believe?
+      <v-container
+        class="px-0"
+        fluid
+      >
+        <mc-radiogroup
+          :radio-options="['Our age estimate', 'Hubble team\'s age estimate']"
+          :feedbacks="['' '']"
+          :correct-answers="[]"
+          :neutral-answers='[0,1]'
+          :selected-callback="(state) => { $emit('ready'); }"
+          score-tag="pro-dat7"
+        >
+        </mc-radiogroup>
+      </v-container>
+    </div>
+    
+    <v-divider
+      class="my-4"
+    >
+    </v-divider>
+
+    <v-row
+      align="center"
+      no-gutters
+    >
+        <v-btn
+          class="black--text"
+          color="accent"
+          elevation="2"
+          @click="
+              () => {
+                state.marker = 'pro_dat6';
+              }
+            "
+        >
+          back
+        </v-btn>
+      <v-spacer></v-spacer>
+      <v-col
+        cols="6"
+        class="shrink"
+        v-if="!state.prodata_response"
+      >
+        <div
+          style="font-size: 16px;"
+        >
+          Choose a response.
+        </div>
+      </v-col>
+      <v-col
+        class="shrink"
+        v-if="state.prodata_response"
+      >
+        <v-btn
+          class="black--text"
+          color="accent"
+          elevation="2"
+          @click="
+              () => {
+                state.marker = 'pro_dat8';
+                state.prodata_response = false;
+              }
+            "
+        >
+          next
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-alert>
+</template>
