885c959b8465d1dc6d3edbf0ab76ed42ed7c3cc0
John Arban Lewis <johnarban@users.noreply.github.com>
2022-12-13 15:00:01 -0500

    add expansion panel to distance tool component and make style name more identifiable

--- src/hubbleds/components/distance_tool/distance_tool.vue
+++ src/hubbleds/components/distance_tool/distance_tool.vue
@@ -69,10 +69,19 @@
         {{ measuring ? 'Stop measuring' : 'Start measuring' }}
       </v-tooltip>
     </div>
-    <contrast-brightness-control 
-        :enabled=true  
-        @newstyle="newstyle => {this.wwtStyle = newstyle}"/>
-        <!-- add inline style to control using inline css like inlineStyle="border: 1px solid white" -->
+    <v-expansion-panels 
+          :flat="true"
+          :tile="true">
+        <v-expansion-panel>
+          <v-expansion-panel-header disable-icon-rotate>Adjust Brightness & Contrast</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <contrast-brightness-control 
+                :enabled=true  
+                @change_style="new_brightness_contrast => {this.wwtStyle = new_brightness_contrast}"/>
+                <!-- add inline style to control using inline css like inlineStyle="border: 1px solid white" -->
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
   </v-card>
 </template>
 
