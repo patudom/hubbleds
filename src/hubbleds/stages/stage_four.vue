c76e4bd417273d9351082ea52bebc9370403cd11
Harry Houghton <henry.houghton@cfa.harvard.edu>
2023-01-25 00:18:05 -0500

    empty plot background colors
    set fill to 'none'.

--- src/hubbleds/stages/stage_four.vue
+++ src/hubbleds/stages/stage_four.vue
@@ -12,6 +12,13 @@
         >
           State
         </v-btn>
+        <v-btn
+          color="error"
+          class="black--text"
+          @click="() => {
+            stage_state.marker = 'con_int2'
+          }"
+        >jump</v-btn>
         Marker: {{ stage_state.marker }}
       </v-col>
     </v-row>
