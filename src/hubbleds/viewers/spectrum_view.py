8f5406474f9eb6852c62f6d124b41c946b967d43
Jon Carifio <carifio24@gmail.com>
2023-04-03 13:47:38 -0400

    Strip what is now mixin functionality out of the spectrum viewer.

--- src/hubbleds/viewers/spectrum_view.py
+++ src/hubbleds/viewers/spectrum_view.py
@@ -1,7 +1,7 @@
 from bqplot import Label
 from bqplot.marks import Lines
 from cosmicds.components.toolbar import Toolbar
-from echo import add_callback, delay_callback, CallbackProperty
+from echo import delay_callback, CallbackProperty
 from glue.config import viewer_tool
 from glue.viewers.common.utils import get_viewer_tools
 from glue.viewers.scatter.state import ScatterViewerState
@@ -9,17 +9,22 @@ from glue_jupyter.bqplot.scatter import BqplotScatterView, \
     BqplotScatterLayerArtist
 from traitlets import Bool
 
+from cosmicds.mixins import LineHoverStateMixin, LineHoverViewerMixin
 from cosmicds.viewers.cds_viewers import cds_viewer
 from ..utils import H_ALPHA_REST_LAMBDA, MG_REST_LAMBDA
 
 __all__ = ['SpectrumView', 'SpectrumViewLayerArtist', 'SpectrumViewerState']
 
 
-class SpectrumViewerState(ScatterViewerState):
+class SpectrumViewerState(LineHoverStateMixin, ScatterViewerState):
     _YMAX_FACTOR = 1.5
 
     resolution_x = CallbackProperty(0)
     resolution_y = CallbackProperty(0)
+
+    def __init__(self, *args, **kwargs):
+        print("SpectrumViewerState __init__")
+        super().__init__(*args, **kwargs)
 
     @property
     def ymax_factor(self):
@@ -47,7 +52,7 @@ class SpectrumViewLayerArtist(BqplotScatterLayerArtist):
                                      self.scatter]
 
 
-class SpecView(BqplotScatterView):
+class SpecView(LineHoverViewerMixin, BqplotScatterView):
     _data_artist_cls = SpectrumViewLayerArtist
     _subset_artist_cls = SpectrumViewLayerArtist
 
@@ -60,85 +65,12 @@ class SpecView(BqplotScatterView):
     observed_text = ' (observed)'
     rest_text = ' (rest)'
 
+    _zoom_tool_ids = "hubble:wavezoom"
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(SpecView, self).__init__(*args, **kwargs)
 
-        self.figure_size_x = 0
-        self.figure_size_y = 230
         self.element = None
-        self._resolution_dirty = True
-
-        self.user_line = Lines(
-            x=[0, 0],
-            y=[0, 0],
-            scales={
-                'x': self.scales['x'],
-                'y': self.scales['y'],
-            },
-            colors=['#1b3e6a'],
-        )
-
-        self.user_line_label = Label(
-            text=[""],
-            x=[],
-            y=[],
-            x_offset=10,
-            y_offset=10,
-            scales={
-                'x': self.scales['x'],
-                'y': self.scales['y'],
-            },
-            colors=['#1b3e6a'],
-        )
-
-        self.label_background = Lines(
-            x=[0, 0],
-            y=[0, 0],
-            stroke_width=25,
-            scales={
-                'x': self.scales['x'],
-                'y': self.scales['y'],
-            },
-            colors=['white'],
-            opacities=[0.8]
-        )
-
-        self.previous_line = Lines(
-            x=[0, 0],
-            y=[0, 0],
-            scales={
-                'x': self.scales['x'],
-                'y': self.scales['y'],
-            },
-            colors=['#a7a5a5'],
-            visible=False
-        )
-
-        self.previous_line_label = Label(
-            text=[""],
-            x=[],
-            y=[],
-            x_offset=10,
-            y_offset=10,
-            scales={
-                'x': self.scales['x'],
-                'y': self.scales['y'],
-            },
-            colors=['#747272'],
-            visible=False
-        )
-
-        self.previous_label_background = Lines(
-            x=[0, 0],
-            y=[0, 0],
-            stroke_width=25,
-            scales={
-                'x': self.scales['x'],
-                'y': self.scales['y'],
-            },
-            colors=['white'],
-            opacities=[0.8]
-        )
 
         self.element_tick = Lines(
             x=[],
@@ -164,123 +96,33 @@ class SpecView(BqplotScatterView):
                 'y': self.scales['y'],
             })
 
-        self.figure.marks += [self.previous_label_background,
-                              self.previous_line, self.previous_line_label,
-                              self.label_background, self.user_line,
-                              self.user_line_label,
-                              self.element_tick, self.element_label]
-
-        self.scale_y.observe(self._update_locations, names=['min', 'max'])
-
-        add_callback(self.state, 'y_min', self._on_ymin_change, echo_old=True)
-        add_callback(self.state, 'y_max', self._on_ymax_change, echo_old=True)
-        add_callback(self.state, 'resolution_x', self._update_x_locations)
-        add_callback(self.state, 'resolution_y', self._update_y_locations)
         self.toolbar.observe(self._active_tool_change, names=['active_tool'])
+
+        print("Finished SpecView __init__")
 
     @staticmethod
     def _label_text(value):
         return f"{value:.0f} Å"
 
-    def _x_background_coordinates(self, x):
-        return [x + 10 * self.state.resolution_x,
-                x + 65 * self.state.resolution_x]
-
-    def _y_background_coordinates(self, y):
-        return [y - 10 * self.state.resolution_y,
-                y - 10 * self.state.resolution_y]
-
     def _active_tool_change(self, change):
         is_tool = change.new is not None
         line_visible = not is_tool or change.new.tool_id != 'hubble:wavezoom'
-        for mark in [self.user_line, self.user_line_label,
+        for mark in [self.line, self.line_label,
                      self.label_background]:
             mark.visible = line_visible
 
-    def _on_ymin_change(self, old, new):
-        if old is not None:
-            ymax = self.state.y_max
-            self.state.resolution_y *= (ymax - new + 20) / (ymax - old + 20)
-        self._update_y_locations()
-
-    def _on_ymax_change(self, old, new):
-        if old is not None:
-            ymin = self.state.y_min
-            self.state.resolution_y *= (new - ymin + 20) / (old - ymin + 20)
-        self._update_y_locations()
-
     def _update_y_locations(self, resolution=None):
+        super()._update_y_locations()
         scale = self.scales['y']
         ymin, ymax = scale.min, scale.max
 
         if ymin is None or ymax is None:
             return
 
-        line_bounds = [ymin, ymax / self.state.ymax_factor]
         tick_bounds = [ymax * 0.74, ymax * 0.87]
         bottom_label_position = ymax * 0.91
-        self.user_line.y = line_bounds
-        self.previous_line.y = line_bounds
-        self.user_line_label.y = [self.user_line.y[1]]
-        self.label_background.y = self._y_background_coordinates(
-            self.user_line_label.y[0])
-        self.previous_line_label.y = [self.previous_line.y[1]]
-        self.previous_label_background.y = self._y_background_coordinates(
-            self.previous_line_label.y[0])
         self.element_tick.y = tick_bounds
         self.element_label.y = [bottom_label_position]
-
-    def _update_x_locations(self, resolution=None):
-        self.user_line_label.x = [self.user_line.x[0]]
-        self.label_background.x = self._x_background_coordinates(
-            self.user_line_label.x[0])
-        self.previous_line_label.x = [self.previous_line.x[0]]
-        self.previous_label_background.x = self._x_background_coordinates(
-            self.previous_line_label.x[0])
-
-    def _update_locations(self, event=None):
-        self._update_x_locations()
-        self._update_y_locations()
-
-    def _on_mouse_moved(self, event):
-
-        if not self.user_line.visible \
-                or self.state.x_min is None:
-            return
-
-        new_x = event['domain']['x']
-
-        if self._resolution_dirty:
-            pixel_x = event['pixel']['x']
-            y = event['domain']['y']
-            pixel_y = event['pixel']['y']
-            self.state.resolution_x = (new_x - self.state.x_min) / pixel_x
-            if self.state.resolution_x != 0:
-                self.figure_size_x = (self.state.x_max - self.state.x_min) / self.state.resolution_x
-            self.state.resolution_y = (self.state.y_max - y) / (
-                        pixel_y - 10)  # The y-axis has 10px "extra" on the top and bottom
-            if self.state.resolution_y != 0:
-                self.figure_size_y = (self.state.y_max - self.state.y_min) / self.state.resolution_y
-            self._resolution_dirty = False
-
-        self.user_line_label.text = [self._label_text(new_x)]
-        self.user_line.x = [new_x, new_x]
-        self.user_line_label.x = [new_x, new_x]
-        self.label_background.x = self._x_background_coordinates(self.user_line_label.x[0])
-        self.label_background.y = self._y_background_coordinates(self.user_line_label.y[0])
-
-    def _on_click(self, event):
-        new_x = event['domain']['x']
-        self.previous_line.x = [new_x, new_x]
-        self.previous_line_label.text = [self._label_text(new_x)]
-        self.previous_line_label.x = [new_x, new_x]
-        self.previous_label_background.x = self._x_background_coordinates(
-            new_x)
-        self.previous_label_background.y = self._y_background_coordinates(
-            self.previous_line_label.y[0])
-        self.previous_line.visible = True
-        self.previous_line_label.visible = True
-        self.previous_label_background.visible = True
 
     def update(self, name, element, z, previous=None):
         self.spectrum_name = name
@@ -292,23 +134,23 @@ class SpecView(BqplotScatterView):
             z > 0)  # The bqplot Mark complained without the explicit bool() call
         self.element_label.visible = items_visible
         self.element_tick.visible = items_visible
-        self.user_line.visible = items_visible
-        self.user_line_label.visible = items_visible
-        self.label_background.visible = items_visible
-        has_previous = previous is not None
-        self.previous_line.visible = has_previous
-        self.previous_line_label.visible = has_previous
-        self.previous_label_background.visible = has_previous
-        if has_previous:
-            self.previous_line.x = [previous, previous]
-            self.previous_line_label.x = [previous, previous]
-            self.previous_line_label.text = [self._label_text(previous)]
-            self.previous_label_background.x = self._x_background_coordinates(
-                previous)
+        # self.line.visible = items_visible
+        # self.line_label.visible = items_visible
+        # self.label_background.visible = items_visible
+        # has_previous = previous is not None
+        # self.previous_line.visible = has_previous
+        # self.previous_line_label.visible = has_previous
+        # self.previous_label_background.visible = has_previous
+        # if has_previous:
+        #     self.previous_line.x = [previous, previous]
+        #     self.previous_line_label.x = [previous, previous]
+        #     self.previous_line_label.text = [self._label_text(previous)]
+        #     self.previous_label_background.x = self._x_background_coordinates(
+        #         previous)
         self.element_label.x = [self.shifted, self.shifted]
         self.element_label.text = [element]
         self.element_tick.x = [self.shifted, self.shifted]
-        self._update_locations()
+        # self._update_locations()
         self._resolution_dirty = True
 
     def add_data(self, data):
@@ -320,13 +162,13 @@ class SpecView(BqplotScatterView):
             if layer.state.layer.label != data.label:
                 layer.state.visible = False
 
-        bring_to_front = [
-            self.previous_label_background, self.previous_line,
-            self.previous_line_label,
-            self.label_background, self.user_line, self.user_line_label
-        ]
-        marks = [x for x in self.figure.marks if x not in bring_to_front]
-        self.figure.marks = marks + bring_to_front
+        # bring_to_front = [
+        #     self.previous_label_background, self.previous_line,
+        #     self.previous_line_label,
+        #     self.label_background, self.line, self.line_label
+        # ]
+        # marks = [x for x in self.figure.marks if x not in bring_to_front]
+        # self.figure.marks = marks + bring_to_front
 
     def initialize_toolbar(self):
         self.toolbar = Toolbar(self)
@@ -342,9 +184,9 @@ class SpecView(BqplotScatterView):
             mode = mode_cls(self)
             self.toolbar.add_tool(mode)
 
-        zoom_tool = self.toolbar.tools["hubble:wavezoom"]
+        #zoom_tool = self.toolbar.tools["hubble:wavezoom"]
 
-        zoom_tool.on_zoom = self.on_xzoom
+        #zoom_tool.on_zoom = self.on_xzoom
 
     def on_xzoom(self, old_state, new_state):
         self.state.resolution_x *= (new_state.x_max - new_state.x_min) / (
@@ -352,7 +194,7 @@ class SpecView(BqplotScatterView):
 
     @property
     def line_visible(self):
-        return self.user_line.visible
+        return self.line.visible
 
 
 SpectrumView = cds_viewer(SpecView, "SpectrumView")
