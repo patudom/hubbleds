3853a218c98b5fd56d0a89976615e311d1852cc6
John Arban Lewis <johnarban@users.noreply.github.com>
2023-04-17 18:20:07 -0400

    create dotplot sequence

---
+++ src/hubbleds/components/generic_state_components/stage_3/guideline_dotplot_seq1.vue
@@ -0,0 +1,32 @@
+<template>
+  <scaffold-alert
+    color="info"
+    class="mb-4 mx-auto"
+    max-width="800"
+    elevation="6"
+    title-text="Measurement Comparison"
+    @back="state.marker_backward = 1"
+    @next="state.marker_forward = 1"
+    :can-advance="(state) => true"
+    :state="state"
+  >
+    <template #before-next>
+      nothing
+    </template>
+
+    <div
+      class="mb-4"
+    >
+      <p>
+        The histogram shows the distrubution of distances measured for this galaxy from you and others who have completed this Data Story. The red dot shows your measurment. 
+      </p>
+    </div>
+  </scaffold-alert>
+</template>
+
+
+<script>
+module.exports = {
+ props: ['state']
+}
+</script>
---
+++ src/hubbleds/components/generic_state_components/stage_3/guideline_dotplot_seq5.vue
@@ -0,0 +1,29 @@
+<template>
+  <scaffold-alert
+    color="info"
+    class="mb-4 mx-auto"
+    max-width="800"
+    elevation="6"
+    title-text="Measurement Comparison"
+    @back="state.marker_backward = 1"
+    @next="state.marker_forward = 1"
+    :can-advance="(state) => true"
+    :state="state"
+  >
+
+    <div
+      class="mb-4"
+    >
+      <p>
+        Click on the galaxy in the second row of the example galaxy table and then take a second measurement considering what you learned in the <span style="background-color: #02838f; border-radius: 5px; padding: 3px; color:white!important;">MEASUREMENT DOS AND DONTS</span>
+      </p>
+    </div>
+  </scaffold-alert>
+</template>
+
+
+<script>
+module.exports = {
+ props: ['state']
+}
+</script>
