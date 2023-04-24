4c7dd81605c35a4367203026dceb9dc8ad6a7dff
Harry Houghton <henry.houghton@cfa.harvard.edu>
2023-04-24 01:03:41 -0400

    set styling standard for previous student responses
    Adds light background, bold font-weight, border-radius. Formatting is a little different depending on whether it appears as a paragraph of text or within one of the StudentResponse elements (which so far are just numeric responses). Main differences are background color and amount of padding.

--- src/hubbleds/components/generic_state_components/stage_5/guideline_your_age_estimate_c.vue
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_your_age_estimate_c.vue
@@ -12,11 +12,12 @@
         How did your drawn trend line compare with your class's best fit line? Were they closer together than the lines for your own five galaxies?
       </p>
       <p>
-        The fit line is labeled with your class's age estimate. How does it compare with the "best guess" age and confidence intervals you identified earlier? Notice whether the best fit age is within the bounds you chose: <StudentResponse class="pa-1 StudentResponses">{{ state.age_calc_state.low_guess }}</StudentResponse> to <StudentResponse class="pa-1 StudentResponses">{{ state.age_calc_state.high_guess }}</StudentResponse> Gyr, with a best guess of <StudentResponse class="pa-1 StudentResponses">{{ state.age_calc_state.best_guess }}</StudentResponse> Gyr.
+        The fit line is labeled with your class's age estimate. How does it compare with the "best guess" age and confidence intervals you identified earlier? Notice whether the best fit age is within the bounds you chose: <StudentResponse class="StudentResponses">{{ state.age_calc_state.low_guess }}</StudentResponse> to <StudentResponse class="StudentResponses">{{ state.age_calc_state.high_guess }}</StudentResponse>&nbsp;Gyr, with a best guess of <StudentResponse class="StudentResponses">{{ state.age_calc_state.best_guess }}</StudentResponse>&nbsp;Gyr.
       </p>
     </div>
   </scaffold-alert>
 </template>
+
 
 <script>
 module.exports = {
