c1f30cf78a647ab2261c02653b0739fa3a8468a0
John Arban Lewis <johnarban@users.noreply.github.com>
2023-04-29 19:29:42 -0400

    switch to check for None

--- src/hubbleds/stages/stage_1.py
+++ src/hubbleds/stages/stage_1.py
@@ -880,7 +880,7 @@ class StageOne(HubbleStage):
     def add_student_velocity(self, table, *args, **kwargs):
         index = table.index
         data_label = table._glue_data.label
-        if hasattr(self.stage_state.student_vel, '__round__'):
+        if self.stage_state.student_vel is None:
             return
         velocity = round(self.stage_state.student_vel)
         self.update_data_value(data_label, VELOCITY_COMPONENT,
