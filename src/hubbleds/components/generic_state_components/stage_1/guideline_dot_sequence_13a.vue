f7a6971ac5177a7b8e8c00ad60545d2cc9e77f03
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-05-01 10:25:57 -0400

    Split this into 2 guidelines

--- src/hubbleds/components/generic_state_components/stage_1/guideline_dot_sequence_13a.vue
+++ src/hubbleds/components/generic_state_components/stage_1/guideline_dot_sequence_13a.vue
@@ -2,20 +2,22 @@
   <scaffold-alert
     @back="() => { state.marker_backward = 1; }"
     @next="() => { state.marker_forward = 1; }"
-
     :state="state"
   >
-      <!-- :can-advance="(state) => state.meas_two_row_selected" -->
+      <!-- :can-advance="(state) => state.meas_two_made" -->
     <template #before-next>
-      Click second row
+      Measure wavelength
     </template>
     <div>
       <p>
-        Click the second row of the example galaxy table. 
+       Remeasure the wavelength of the spectral line keeping in mind what you learned from the dot plots. 
       </p>
     </div>
   </scaffold-alert>
 </template>
+
+
+
 
 <script>
 module.exports = {
