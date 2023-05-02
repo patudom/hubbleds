0241629ed122763cd2689685ac5313dfd8ac7289
John Arban Lewis <johnarban@users.noreply.github.com>
2023-05-02 10:32:01 -0400

    remove pro_dat5 question

--- src/hubbleds/components/generic_state_components/stage_6/guideline_professional_data5.vue
+++ src/hubbleds/components/generic_state_components/stage_6/guideline_professional_data5.vue
@@ -14,9 +14,6 @@
         This data is from the HST Key Project, led by astronomer Wendy Freedman, who used data from the Hubble Space Telescope to measure a quantity known as the <strong>Hubble constant</strong>
         (which is the inverse of the age value you calculated).
       </p>
-      <p> 
-        Whose data has the least amount of random uncertainty? (It might help to use the magnifying glass tool to zoom in on the x and y axes).
-      </p>
     </div>
   </guideline-professional-data>
 </template>
