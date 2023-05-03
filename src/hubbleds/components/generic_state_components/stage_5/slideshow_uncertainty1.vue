1194a57fa8369b264d4a7ab27420fb7b372fb15d
John Arban Lewis <johnarban@users.noreply.github.com>
2023-05-03 13:13:47 -0400

    modify citation

--- src/hubbleds/components/generic_state_components/stage_5/slideshow_uncertainty1.vue
+++ src/hubbleds/components/generic_state_components/stage_5/slideshow_uncertainty1.vue
@@ -68,7 +68,13 @@
                     contain
                     src="https://picsum.photos/600/600"
                   ></v-img>
-                  <cite class="text-center mt-2 grey--text">Vesto Slipher's spectrum of the Andromeda Galaxy  from Lowell Observatory <span style="display: none">https://ui.adsabs.harvard.edu/abs/2009JAHH...12...72B/abstract</span></cite>
+                  <cite class="text-center mt-2 grey--text">
+                    Vesto Slipher's spectrum of the Andromeda Galaxy  from Lowell Observatory 
+                    <span style="display: none">
+                      <!-- credit url https://ui.adsabs.harvard.edu/abs/2009JAHH...12...72B/abstract -->
+                      Source: Modified from <a href="https://ui.adsabs.harvard.edu/abs/2009JAHH...12...72B/abstract">Br&eacture;mond, Journal of Astronomical History and Heritage, 2009</a>
+                    </span>
+                  </cite>
                 </div>
                 </v-col>
                 
