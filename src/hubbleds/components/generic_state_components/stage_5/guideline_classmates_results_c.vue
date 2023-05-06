f70d6d4a2b3e4a2305e37ab9b5da33b85945eeec
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-05-06 18:11:27 -0400

    Add note to zoom in to avoid outliers in the viewers with sliders

--- src/hubbleds/components/generic_state_components/stage_5/guideline_classmates_results_c.vue
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_classmates_results_c.vue
@@ -10,19 +10,14 @@
       <p>
         Now let’s look at age estimates of the universe from other classes who have already completed this data story.
       </p>
-      <v-card
-        color="secondary"
-        class="mb-4"
-      >
-        <v-card-text>
-          Full disclosure for our pilot testers: Since there were not many students before you, the CosmicDS team acted as "seed students," created galaxies measurements as you have, and grouped our data into "classes."
-        </v-card-text>
-      </v-card>
       <p>
         Let’s look at the data, class by class. Some class measured the lowest age value for the universe. Some class measured the highest age.
       </p>
       <p>
         Use the slider to scan through the age estimates that each participating class measured. 
+      </p>
+      <p>
+        To exclude outliers, click <v-btn icon tile dark x-small disabled class="mx-1" elevation="2" style="background-color: #0277BD; border-radius: 5px;"><v-icon style="color:white!important;">mdi-select-search</v-icon></v-btn> to zoom in to a smaller velocity or distance range.
       </p>
     </div>
   </scaffold-alert>
