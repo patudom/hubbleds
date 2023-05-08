67cc72368bddc30ad4c583805248758becb0f8eb
John Arban Lewis <johnarban@users.noreply.github.com>
2023-05-07 23:02:33 -0400

    hide labels on pro_dat1

--- src/hubbleds/stages/stage_6.py
+++ src/hubbleds/stages/stage_6.py
@@ -141,6 +141,7 @@ class StageFive(HubbleStage):
         
         prodata_viewer = self.add_viewer(HubbleScatterView, "prodata_viewer",
                                          "Professional Data")
+        prodata_viewer.toolbar.tools["hubble:linefit"].show_labels = False
         prodata_viewer.toolbar.set_tool_enabled("hubble:linefit", self.stage_state.marker_reached("pro_dat1"))
         prodata_viewer.figure.axes[1].label_offset = "5em"
 
