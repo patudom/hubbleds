d09f57e3e24bb27d7c5224d8b4c10a14ce200ae9
John Arban Lewis <johnarban@users.noreply.github.com>
2023-05-19 13:52:42 -0400

    add warning at end of stage 1 if they have a bad velocity

--- src/hubbleds/components/generic_state_components/stage_1/guideline_end_stage1.vue
+++ src/hubbleds/components/generic_state_components/stage_1/guideline_end_stage1.vue
@@ -8,6 +8,15 @@
     <div>
       <p>Great work! You have completed Stage 1. Proceed to Stage 2.</p>
     </div>
+    
+    <v-card
+      v-if="state.has_bad_velocities"
+      color="warning"
+      >
+      <v-card-text>
+        <strong>Warning:</strong>You seem to have some bad measurements. Please review your measurements and try again. If you don't see an issue, ask your teacher for help.
+      </v-card-text>
+    </v-card>
   </scaffold-alert>
 </template>
 
