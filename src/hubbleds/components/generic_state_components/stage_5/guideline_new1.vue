25c481055d64aab3d9512b064e9f0bf10103eef2
John Arban Lewis <johnarban@users.noreply.github.com>
2023-07-08 19:29:01 -0400

    write new guidelines

---
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_new1.vue
@@ -0,0 +1,25 @@
+<template>
+  <scaffold-alert
+    title-text="Select Best Value"
+    @back="state.marker_backward = 1"
+    @next="state.marker_forward = 1"
+  >
+    <div
+      class="mb-4"
+    >
+    <p>
+      When scientists make a measurement of an unknown quantity, they report the value that they think is most likely based on their measurements, and they report something called a confidence interval which gives lower and upper bounds on the most plausible range of measurement values.
+    </p>
+    <p>
+      We'll guide you through how to do this for your class's measurements.
+    </p>
+    </div>
+  </scaffold-alert>
+</template>
+
+
+<script>
+module.exports = {
+  props: ['state']
+}
+</script>
---
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_new5.vue
@@ -0,0 +1,25 @@
+<template>
+  <scaffold-alert
+    title-text="Select Best Value"
+    @back="state.marker_backward = 1"
+    @next="state.marker_forward = 1"
+  >
+    <div
+      class="mb-4"
+    >
+    <p>
+      Now that you’ve identified your most likely value for the age of the universe, 
+      we need to acknowledge that it’s not the only possible value. 
+      Other values that were measured by your classmates are also possible, 
+      so we want to specify a likely range of age values.
+    </p>
+    </div>
+  </scaffold-alert>
+</template>
+
+
+<script>
+module.exports = {
+  props: ['state']
+}
+</script>
