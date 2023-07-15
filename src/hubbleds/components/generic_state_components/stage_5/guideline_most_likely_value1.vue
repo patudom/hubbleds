79474d0094465af10ea58d3b05e31f53a49d1779
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-07-14 22:19:12 -0400

    Edit original text (that was duplicated in new1)

---
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_most_likely_value1.vue
@@ -0,0 +1,34 @@
+<template>
+  <scaffold-alert
+    title-text="Most Likely Values"
+    @back="state.marker_backward = 1"
+    @next="state.marker_forward = 1"
+  >
+    <div
+      class="mb-4"
+    >
+      <p>
+        When scientists measure an unknown quantity, they use the available data to determine: 
+        <ol>
+          <li>the measured value they think is <strong>most likely</strong>, and </li>
+          <li>the most plausible range of measured values (called the <strong>confidence interval</strong>). </li> 
+        </ol>
+      </p>
+      <p>
+        Let's explore how to do this for your class's measurements.
+      </p>
+    </div>
+  </scaffold-alert>
+</template>
+
+<style>
+  .JaxEquation .MathJax {
+    margin: 3px auto !important;
+  }
+</style>
+
+<script>
+module.exports = {
+  props: ['state']
+}
+</script>
