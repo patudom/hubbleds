9f9497d0c7c033a09de4ceede2e01ff2df8d3300
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-07-14 22:23:12 -0400

    Rename and edit text for new6.
    Most of the deleted text from new6 is in the hint on con_int3

---
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_confidence_interval2.vue
@@ -0,0 +1,29 @@
+<template>
+  <scaffold-alert
+    title-text="Select Best Value"
+    @back="state.marker_backward = 1"
+    @next="state.marker_forward = 1"
+  >
+    <div
+      class="mb-4"
+    >
+    <p>
+      Some possibilities are the range that encompass the inner 50%, 68%, or 95% of all measurements.
+    </p>
+    <p>
+      Click the radio buttons to display each percentage range in the histogram. 
+    </p>
+    <p>
+      Click <v-icon>mdi-help-circle-outline</v-icon> to learn more about how the percentage ranges are determined.
+    </p>
+
+    </div>
+  </scaffold-alert>
+</template>
+
+
+<script>
+module.exports = {
+  props: ['state']
+}
+</script>
