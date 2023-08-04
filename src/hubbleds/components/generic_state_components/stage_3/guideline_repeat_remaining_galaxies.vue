603477bddadfbb3c83e4fbe433b4edafe57bdc01
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-08-04 09:51:35 -0400

    Update repeating measurement guideline to match Stage 1

--- src/hubbleds/components/generic_state_components/stage_3/guideline_repeat_remaining_galaxies.vue
+++ src/hubbleds/components/generic_state_components/stage_3/guideline_repeat_remaining_galaxies.vue
@@ -11,20 +11,42 @@
     :state="state"
   >
     <template #before-next>
-      Measure angular size<span v-if="state.angsizes_total < 4">s</span> for {{ 5 - state.angsizes_total }} more <span v-if="state.angsizes_total < 4">galaxies</span><span v-if="state.angsizes_total == 4">galaxy</span>.
+      <span v-if="!state.bad_angsize">
+        Measure angular size<span v-if="state.angsizes_total < 4">s</span> for {{ 5 - state.angsizes_total }} more <span v-if="state.angsizes_total < 4">galaxies</span><span v-if="state.angsizes_total == 4">galaxy</span>.
+      </span>
+      <span v-if="state.bad_angsize">
+        <strong>Remeasure angular size</strong>
+        <br/>
+      </span>
     </template>
 
+    <!-- If there are no bad measurements and measurements are not complete -->
     <div
       class="mb-4"
-      v-if="state.angsizes_total < 5"
+      v-if="state.angsizes_total < 5 && !state.bad_angsize"
     >
       <p>
         Repeat the angular size measurements for each of the remaining galaxies in your table.
       </p>
     </div>
+
+    <!-- If there are any bad measurements -->
     <div
       class="mb-4"
-      v-if="state.angsizes_total >= 5"
+      v-if="state.bad_angsize"
+    >
+      <p>
+        Your measured angular size is not within the expected range. Please try again.
+      </p>
+      <p>
+        Ask your instructor if you are not sure where to measure.
+      </p>
+    </div>
+
+    <!-- If measurements are complete and there are no bad measurements -->  
+    <div
+      class="mb-4"
+      v-if="state.angsizes_total >= 5 && !state.bad_angsize"
     >
       <p>
         You have measured angular sizes for all of your galaxies.
