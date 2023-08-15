fe1122503e4d4f405c9a8f861c857e857388655e
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-08-15 18:58:32 -0400

    Text edit

--- src/hubbleds/components/generic_state_components/stage_5/guideline_finished_classmates.vue
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_finished_classmates.vue
@@ -7,7 +7,7 @@
     <div
     >
       <p>
-        Note: "Your class" refers to the {{ state.class_data_size - 1 }} others who have completed their measurements (and might not represent your entire class). To avoid confusion, we will <strong>not</strong> add new measurements as they come in, so you may obtain different results from classmates who complete their measurements after you.
+        Note: "Your class" refers to you and the {{ state.class_data_size - 1 }} others who have completed their measurements (and might not represent your entire class). To avoid confusion, we will <strong>not</strong> add new measurements as they come in, so you may obtain different results from classmates who complete their measurements after you.
       </p>
     </div>
   </scaffold-alert>
