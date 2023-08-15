0910b0b846710537e194114447257be7a1b29fe5
Pat Udomprasert <pudompra@cfa.harvard.edu>
2023-08-15 18:41:46 -0400

    Minor edits to shorten/clarify text; remove instruction about outliers since they won't see those with measurement guardrails.

--- src/hubbleds/components/generic_state_components/stage_5/guideline_classmates_results.vue
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_classmates_results.vue
@@ -8,7 +8,7 @@
       class="mb-4"
     >
       <p>
-        Let’s look at your classmates' data, student by student. Someone in your class measured the lowest age value for the universe. Someone measured the highest age.
+        Let’s look at your class's data, student by student. Someone in your class measured the lowest age value for the universe. Someone measured the highest age.
       </p>
       <p>
         Use the slider to scan through the age estimates that each person in your class measured. 
@@ -17,7 +17,7 @@
         Your value appears in orange-red, and your classmates' are blue.
       </p>
       <p>
-        To exclude outliers, click <v-btn icon tile dark x-small disabled class="mx-1" elevation="2" style="background-color: #0277BD; border-radius: 5px;"><v-icon style="color:white!important;">mdi-select-search</v-icon></v-btn> to zoom in to a smaller velocity or distance range.
+        If you like, click <v-btn icon tile dark x-small disabled class="mx-1" elevation="2" style="background-color: #0277BD; border-radius: 5px;"><v-icon style="color:white!important;">mdi-select-search</v-icon></v-btn> to zoom in to a specific velocity or distance range.
       </p>
     </div>
   </scaffold-alert>
--- src/hubbleds/components/generic_state_components/stage_5/guideline_classmates_results_c.vue
+++ src/hubbleds/components/generic_state_components/stage_5/guideline_classmates_results_c.vue
@@ -16,9 +16,6 @@
       <p>
         Use the slider to scan through the age estimates that each participating class measured. 
       </p>
-      <p>
-        To exclude outliers, click <v-btn icon tile dark x-small disabled class="mx-1" elevation="2" style="background-color: #0277BD; border-radius: 5px;"><v-icon style="color:white!important;">mdi-select-search</v-icon></v-btn> to zoom in to a smaller velocity or distance range.
-      </p>
     </div>
   </scaffold-alert>
 </template>
