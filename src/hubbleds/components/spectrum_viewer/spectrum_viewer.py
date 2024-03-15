3ad22045ea821b0473d938ffcb06cceca9279fcc
Nicholas Earl <contact@nicholasearl.me>
2024-03-15 09:13:00 -0500

    Allow for passing in spectrum data to render

---
+++ src/hubbleds/components/spectrum_viewer/spectrum_viewer.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import solara
+import reacton.ipyvuetify as rv
+from ipyvuetify import VuetifyTemplate
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+from astropy.table import Table
+
+
+df = Table(
+    {"wave": np.linspace(3000, 8000, 1000), "flux": np.random.sample(1000)}
+).to_pandas()
+
+
+@solara.component
+def SpectrumViewer(data=pd.DataFrame()):
+    with rv.Card():
+        with rv.Toolbar(color="primary", dense=True):
+            with rv.ToolbarTitle():
+                solara.Text("MY GALAXIES")
+
+        fig = px.line(data, x="wave", y="flux")
+
+        fig.add_hline(
+            y=1,
+            line_width=0.5,
+            line_dash="dot",
+            line_color="cyan",
+            annotation_text="1BASE",
+            annotation_font_size=12,
+            annotation_position="bottom right",
+        )
+
+        fig.update_layout(
+            xaxis_zeroline=False,
+            yaxis_zeroline=False,
+            xaxis=dict(
+                spikecolor="white",
+                spikethickness=1,
+                spikedash="solid",
+                spikemode="across",
+                spikesnap="cursor",
+            ),
+            spikedistance=-1,
+            #     hoverdistance=0,
+            hovermode="x",
+        )
+
+        solara.FigurePlotly(
+            fig,
+        )
