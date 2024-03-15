2f9429f0d77a3e5ba4650a44901ac9909f5a6c7c
Nicholas Earl <contact@nicholasearl.me>
2024-03-15 09:14:27 -0500

    Update guideline vue files for solara compatibility

---
+++ src/hubbleds/pages/01-spectra-&-velocity/guidelines/GuidelineSelectGalaxies1.vue
@@ -0,0 +1,22 @@
+<template>
+  <scaffold-alert
+    color="info"
+    class="mb-4 mx-auto"
+    max-width="800"
+    elevation="6"
+    title-text="Select Your Galaxies"
+    @back="back_callback()"
+    @next="next_callback()"
+  >
+    <div
+      class="mb-4"
+    >
+      <p>
+        This is a view of the cosmic sky, like the one you explored in the introduction. This view shows a more modern data set from the Sloan Digital Sky Survey (SDSS), which has collected imaging and spectral data for millions of galaxies. 
+      </p>
+      <p>
+        This Data Story will focus on a nearby subset of spiral galaxies.
+      </p>
+    </div>
+  </scaffold-alert>
+</template>
