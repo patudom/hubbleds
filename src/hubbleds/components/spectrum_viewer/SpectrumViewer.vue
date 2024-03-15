76f6a99f038d21c0c6c5654591bd1440500a0e61
Nicholas Earl <contact@nicholasearl.me>
2024-03-15 09:13:00 -0500

    Allow for passing in spectrum data to render

---
+++ src/hubbleds/components/spectrum_viewer/SpectrumViewer.vue
@@ -0,0 +1,47 @@
+<script>
+import Plotly from 'plotly.dist';
+
+export default {
+  name: "SpectrumViewer",
+  props: ["chart"],
+  mounted() {
+    Plotly.plot(this.$refs[this.chart.uuid], this.chart.traces, this.chart.layout);
+  },
+  data: () => ({
+    chart: {
+      uuid: "123",
+      traces: [
+        {
+          y: [],
+          line: {
+            color: "#5e9e7e",
+            width: 4,
+            shape: "line"
+          }
+        }
+      ],
+      layout: {}
+    }
+  }),
+  watch: {
+    chart: {
+      handler: function() {
+        Plotly.react(
+          this.$refs[this.chart.uuid],
+          this.chart.traces,
+          this.chart.layout
+        );
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<template>
+<div :ref="chart.uuid"></div>
+</template>
+
+<style scoped>
+
+</style>
