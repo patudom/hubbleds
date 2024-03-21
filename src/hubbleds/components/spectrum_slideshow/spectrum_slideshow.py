12ba48fdf40a557a69653fbc7c80770e536b7e92
Nicholas Earl <contact@nicholasearl.me>
2024-03-21 11:31:59 -0500

    Solara-ify spectrum slideshow component

---
+++ src/hubbleds/components/spectrum_slideshow/spectrum_slideshow.py
@@ -0,0 +1,15 @@
+import solara
+import reacton.ipyvuetify as rv
+from ipyvuetify import VuetifyTemplate
+from ...utils import IMAGE_BASE_URL
+
+
+@solara.component_vue("SpectrumSlideshow.vue")
+def SpectrumSlideshow(
+    step=0,
+    length=11,
+    dialog=False,
+    opened=False,
+    image_location=f"{IMAGE_BASE_URL}/stage_one_spectrum",
+):
+    pass
