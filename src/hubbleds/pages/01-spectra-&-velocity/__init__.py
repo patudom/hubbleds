63d7d420d5baddca39ce3d3f5c89a18514cf41fb
Nicholas Earl <contact@nicholasearl.me>
2024-03-21 11:33:27 -0500

    Add basic debug mode shortcuts

--- src/hubbleds/pages/01-spectra-&-velocity/__init__.py
+++ src/hubbleds/pages/01-spectra-&-velocity/__init__.py
@@ -10,10 +10,11 @@ from glue_jupyter.app import JupyterApplication
 from reacton import ipyvuetify as rv
 from solara import Reactive
 from pathlib import Path
+from astropy.table import Table
 
-from ...components import DataTable, SpectrumViewer
+from ...components import DataTable, SpectrumViewer, SpectrumSlideshow
 from ...data_management import *
-from ...state import GLOBAL_STATE
+from ...state import GLOBAL_STATE, LOCAL_STATE
 from ...widgets.selection_tool import SelectionTool
 from ...data_models.student import student_data, StudentMeasurement, example_data
 from .component_state import ComponentState, Marker
@@ -26,26 +27,15 @@ gjapp = JupyterApplication(GLOBAL_STATE.data_collection, GLOBAL_STATE.session)
 component_state = ComponentState()
 
 
-def _on_galaxy_selected(state, galaxy):
+def _on_galaxy_selected(galaxy):
     is_in = np.isin(
         [x.name for x in student_data.measurements], galaxy["name"]
     )  # Avoid duplicates
     already_present = is_in.size > 0 and is_in[0]
 
     if not already_present:
-        filename = galaxy["name"]
-        gal_type = galaxy["type"]
-
-        print(galaxy)
-
-        # if not self._filling_data:
-        #     galaxy.pop("element")
-        #
-        # self.story_state.load_spectrum_data(filename, gal_type)
-        # self.add_data_values(STUDENT_MEASUREMENTS_LABEL, galaxy)
-        # self.stage_state.galaxy = galaxy
         student_data.measurements.append(StudentMeasurement(**galaxy))
-        state.total_galaxies.value += 1
+        component_state.total_galaxies.value += 1
 
 
 def _on_example_galaxy_table_row_selected(row):
@@ -67,8 +57,20 @@ def Page():
     solara.Text(
         f"Current step: {component_state.current_step.value}, "
         f"Can advance: {component_state.can_transition(next=True)}"
-        f"{component_state.selected_example_galaxy}"
     )
+
+    if LOCAL_STATE.debug_mode:
+
+        def _on_select_galaxies_clicked():
+            gal_tab = Table(component_state.galaxy_data)
+            gal_tab["id"] = [str(x) for x in gal_tab["id"]]
+            for i in range(5 - component_state.total_galaxies.value):
+                gal = dict(gal_tab[i])
+                _on_galaxy_selected(gal)
+
+            component_state.transition_to(Marker.sel_gal3, force=True)
+
+        solara.Button("Select 5 Galaxies", on_click=_on_select_galaxies_clicked)
 
     with rv.Row():
         with rv.Col(cols=4):
@@ -128,7 +130,7 @@ def Page():
                 """Store a galaxy that has been clicked in the selection tool"""
                 selection_tool_widget = solara.get_widget(selection_tool)
                 selection_tool_widget.on_galaxy_selected = (
-                    lambda gal: _on_galaxy_selected(component_state, gal)
+                    lambda gal: _on_galaxy_selected(gal)
                 )
                 selection_tool_widget.observe(
                     lambda gal: component_state.selected_galaxy.set(gal["new"]),
@@ -197,7 +199,7 @@ def Page():
                         },
                         {"text": "Velocity (km/s)", "value": "velocity"},
                     ],
-                    items=example_data.model_dump()["measurements"],
+                    items=example_data.model_dump(exclude=["spectrum"])["measurements"],
                     highlighted=component_state.is_current_step(Marker.cho_row1),
                     event_on_row_selected=_on_example_galaxy_table_row_selected,
                 )
@@ -222,7 +224,7 @@ def Page():
                         },
                         {"text": "Velocity (km/s)", "value": "velocity"},
                     ],
-                    items=student_data.model_dump()["measurements"],
+                    items=student_data.model_dump(exclude=["spectrum"])["measurements"],
                     highlighted=component_state.is_current_step(Marker.not_gal_tab),
                 )
 
@@ -242,12 +244,18 @@ def Page():
 
         with rv.Col(cols=8):
             if component_state.is_current_step(Marker.mee_spe1):
-                from astropy.table import Table
+                spec_data = example_data.model_dump()["measurements"][0]["spectrum"]
 
-                df = Table(
-                    {
-                        "wave": np.linspace(3000, 8000, 1000),
-                        "flux": np.random.sample(1000),
-                    }
-                ).to_pandas()
-                spectrum_viewer = SpectrumViewer(df)
+                spectrum_viewer = SpectrumViewer(
+                    Table(
+                        {"wave": spec_data["wave"], "flux": spec_data["flux"]}
+                    ).to_pandas()
+                )
+
+    with rv.Row():
+        with rv.Col(cols=4):
+            pass
+
+        with rv.Col(cols=8):
+            if component_state.current_step.value.value >= Marker.mee_spe1.value:
+                SpectrumSlideshow()
