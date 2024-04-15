ed71edd1a6c83dc43949374ee250f6ae57844f30
Nicholas Earl <contact@nicholasearl.me>
2024-04-15 18:49:21 -0500

    Add doppler slide show step

--- src/hubbleds/components/__init__.py
+++ src/hubbleds/components/__init__.py
@@ -1,3 +1,4 @@
 from .data_table.data_table import DataTable
 from .spectrum_viewer.spectrum_viewer import SpectrumViewer
 from .spectrum_slideshow.spectrum_slideshow import SpectrumSlideshow
+from .doppler_slideshow.doppler_slideshow import DopplerSlideshow
