fcba9978cb2594c18aee6c41d9a46f44e680d4f4
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-05-01 11:50:24 -0400

    Move intro slideshow content to component

--- src/hubbleds/components/__init__.py
+++ src/hubbleds/components/__init__.py
@@ -1,3 +1,4 @@
+from .intro_slideshow.intro_slideshow import IntroSlideshow
 from .data_table.data_table import DataTable
 from .spectrum_viewer.spectrum_viewer import SpectrumViewer
 from .spectrum_slideshow.spectrum_slideshow import SpectrumSlideshow
