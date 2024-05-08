295aa68007add69d93c8c1b7dee4e2fc580fe37c
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-05-08 18:28:08 -0400

    Will have to update these once we wire up student distance measurements

--- src/hubbleds/pages/03-distance-measurements/guidelines/GuidelineFillRemainingGalaxies.vue
+++ src/hubbleds/pages/03-distance-measurements/guidelines/GuidelineFillRemainingGalaxies.vue
@@ -3,13 +3,18 @@
     title-text="Estimate Distance"
     next-text="stage 4"
     @back="back_callback()"
-    @next="next_callback()"
-    :can-advance="can_advance"
   >
     <template #before-next>
       Click <v-btn icon tile dark x-small disabled class="mx-1" elevation="2" style="background-color: #0277BD; border-radius: 5px;"><v-icon style="color:white!important;">mdi-tape-measure</v-icon></v-btn> in toolbar
     </template>
 
+    <v-card color="error">
+      <v-card-text>
+        This guideline needs to be fixed once we wire up student measurements
+      </v-card-text>
+    </v-card>
+
+  <!-- FIX when students have measurements 
     <div
       v-if="state.distances_total < 5"
       class="mb-4"
@@ -26,6 +31,6 @@
         class="mb-4"
     >
       <p>Distances have been entered in the table. Proceed to Stage 4.</p>
-    </div>
+    </div> -->
   </scaffold-alert>
 </template>
