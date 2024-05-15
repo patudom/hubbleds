d499872fe36f0cd2b4a7163bc94c7176eb065ac9
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-05-15 17:33:46 -0400

    Add component_state and lay out guidelines in solara page

---
+++ src/hubbleds/pages/04-explore-data/component_state.py
@@ -0,0 +1,107 @@
+from solara import Reactive
+import solara
+import enum
+from ...decorators import computed_property
+import dataclasses
+from cosmicds.utils import API_URL
+from ...state import GLOBAL_STATE
+from ...data_models.student import example_data, StudentMeasurement, SpectrumData
+from contextlib import closing
+from io import BytesIO
+from astropy.io import fits
+
+
+ELEMENT_REST = {
+    'H-α': 6562.79,
+    'Mg-I': 5176.7
+}
+
+
+class Marker(enum.Enum):
+    exp_dat1 = enum.auto()
+    tre_dat1 = enum.auto()
+    tre_dat2 = enum.auto()
+    tre_dat3 = enum.auto()
+    rel_vel1 = enum.auto()
+    hub_exp1 = enum.auto()
+    tre_lin1 = enum.auto()
+    tre_lin2 = enum.auto()
+    bes_fit1 = enum.auto()
+    age_uni1 = enum.auto()
+    hyp_gal1 = enum.auto()
+    age_rac1 = enum.auto()
+    age_uni2 = enum.auto()
+    age_uni3 = enum.auto()
+    age_uni4 = enum.auto()
+    you_age1 = enum.auto()
+    sho_est1 = enum.auto()
+    sho_est2 = enum.auto()
+
+    @staticmethod
+    def next(step):
+        return Marker(step.value + 1)
+
+    @staticmethod
+    def previous(step):
+        return Marker(step.value - 1)
+
+    @staticmethod
+    def first():
+        return Marker(1)
+    
+    @staticmethod
+    def last():
+        return Marker(len(Marker))
+
+
+@dataclasses.dataclass
+class ComponentState:
+    current_step: Reactive[Marker] = dataclasses.field(
+        default=Reactive(Marker.exp_dat1)
+    )
+    dosdonts_tutorial_opened: Reactive[bool] = dataclasses.field(
+        default=Reactive(False)
+    )
+
+    def setup(self):
+        pass
+
+    def is_current_step(self, step: Marker):
+        return self.current_step.value == step
+
+    def can_transition(self, step: Marker = None, next=False, prev=False):
+        if next:
+            if self.current_step.value is Marker.last():
+                return False  # FIX once we sort out transitions between stages
+            step = Marker.next(self.current_step.value)
+        elif prev:
+            if self.current_step.value is Marker.first():
+                return False  # FIX once we sort out transitions between stages
+            step = Marker.previous(self.current_step.value)
+
+        if hasattr(self, f"{step.name}_gate"):
+            return getattr(
+                self,
+                f"{step.name}_gate",
+            )().value
+
+        print(f"No gate exists for step {step.name}, allowing anyway.")
+        return True
+
+    def transition_to(self, step: Marker, force=False):
+        if self.can_transition(step) or force:
+            self.current_step.set(step)
+        else:
+            print(
+                f"Conditions not met to transition from "
+                f"{self.current_step.value.name} to {step.name}."
+            )
+
+    def transition_next(self):
+        next_marker = Marker.next(self.current_step.value)
+        self.transition_to(next_marker)
+
+    def transition_previous(self):
+        previous_marker = Marker.previous(self.current_step.value)
+        self.transition_to(previous_marker, force=True)
+
