1a9940dc91fa5337efb63acca84e2555c9fca341
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-05-23 17:28:24 -0400

    Make it easier to check marker range for stage layout

--- src/hubbleds/marker_base.py
+++ src/hubbleds/marker_base.py
@@ -18,3 +18,10 @@ class MarkerBase(metaclass=EnumMeta):
     @classmethod
     def last(cls):
         return cls(len(cls))
+    
+    @classmethod
+    # check if the current_step is inclusively within the start and end markers specified. If no end is specified, use the final marker.
+    def incl_range(cls, component_state, start, end=None):
+        if end == None:
+            end = cls.last()
+        return (component_state.current_step.value.value >= start.value and component_state.current_step.value.value <= end.value )
