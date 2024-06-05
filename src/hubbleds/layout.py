f9e2c587e4d49fc252d17444c3d6c4875db9044d
Nicholas Earl <contact@nicholasearl.me>
2024-06-05 18:45:45 -0500

    Refactor layout to support argument passing

---
+++ src/hubbleds/layout.py
@@ -0,0 +1,17 @@
+from cosmicds.layout import Layout as BaseLayout
+import solara
+from .state import GLOBAL_STATE
+from cosmicds.components import MathJaxSupport
+
+
+@solara.component
+def Layout(children=[]):
+
+    # Mount external javascript libraries
+    def _load_math_jax():
+        MathJaxSupport()
+
+    solara.use_memo(_load_math_jax, dependencies=[])
+
+    with BaseLayout(children=children, story_title="Hubble's Law"):
+        pass
