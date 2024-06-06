2b6aa275c695613a48a1262c8b0db2c803769e9f
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-06-06 17:58:27 -0400

    Add before-next prompt (and remove boilerplate formatting)

--- src/hubbleds/pages/06-prodata/guidelines/GuidelineProfessionalData9.vue
+++ src/hubbleds/pages/06-prodata/guidelines/GuidelineProfessionalData9.vue
@@ -1,15 +1,14 @@
 <!-- # multiple choice -->
 <template>
   <scaffold-alert
-    color="info"
-    class="mb-4 mx-auto"
-    max-width="800"
-    elevation="6"
     @back="back_callback()"
     @next="next_callback()"
     :can-advance="can_advance"
     
   >
+    <template #before-next>
+      Choose a response.
+    </template>
     <div
       class="mb-4"
     >
