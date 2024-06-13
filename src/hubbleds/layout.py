3a74e668979b26e320aae47c3b4d3b95fc9cb977
Nicholas Earl <contact@nicholasearl.me>
2024-06-13 02:32:52 -0500

    Load database measurements on story load

--- src/hubbleds/layout.py
+++ src/hubbleds/layout.py
@@ -2,7 +2,7 @@ from cosmicds.layout import BaseLayout
 from cosmicds.state import GLOBAL_STATE
 import solara
 from cosmicds.components import MathJaxSupport, PlotlySupport
-from .data_models.student import student_data, StudentMeasurement
+from .data_models.student import student_data, StudentMeasurement, example_data
 from .remote import DatabaseAPI
 
 
@@ -23,6 +23,17 @@ def Layout(children=[]):
         def _load_student_measurements():
             stored_measurements = DatabaseAPI.get_measurements()
             student_data.measurements = stored_measurements
+
+            stored_example_measurements = DatabaseAPI.get_measurements(samples=True)
+
+            if len(stored_example_measurements) > 0:
+                example_data.measurements = stored_example_measurements
+            else:
+                print("No stored sample measurements; creating new one...")
+                example_data.measurements = [StudentMeasurement(
+                    **DatabaseAPI.get_sample_galaxy()
+                )]
+                DatabaseAPI.put_measurements(True)
 
         solara.use_memo(
             _load_student_measurements, dependencies=[GLOBAL_STATE.student.id.value]
