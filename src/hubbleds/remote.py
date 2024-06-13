e64bfa56d53cdf39d37f6f9d6b19664bde41e0b1
Nicholas Earl <contact@nicholasearl.me>
2024-06-13 02:33:41 -0500

    Expand database proxy methods, formalize spectrum parsing

--- src/hubbleds/remote.py
+++ src/hubbleds/remote.py
@@ -1,7 +1,7 @@
 from cosmicds.utils import API_URL
 from cosmicds.state import GLOBAL_STATE
 from .utils import HUBBLE_ROUTE_PATH
-from .data_models.student import student_data, StudentMeasurement
+from .data_models.student import student_data, StudentMeasurement, example_data
 from contextlib import closing
 from io import BytesIO
 from astropy.io import fits
@@ -9,84 +9,93 @@ from .state import LOCAL_STATE
 import datetime
 
 
+ELEMENT_REST = {"H-α": 6562.79, "Mg-I": 5176.7}
+
+
 class DatabaseAPI:
     @staticmethod
-    def get_measurements():
-        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/measurements/{GLOBAL_STATE.student.id.value}"
+    def _load_spectrum_data(gal_info):
+        file_name = f"{gal_info['name'].replace('.fits', '')}.fits"
+        gal_type = gal_info["type"]
+
+        print(file_name)
+
+        type_folders = {"Sp": "spiral", "E": "elliptical", "Ir": "irregular"}
+        folder = type_folders[gal_type]
+        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/spectra/{folder}/{file_name}"
+        response = GLOBAL_STATE.request_session.get(url)
+
+        print(response)
+
+        with closing(BytesIO(response.content)) as f:
+            f.name = gal_info["name"]
+
+            with fits.open(f) as hdulist:
+                data = hdulist["COADD"].data if "COADD" in hdulist else None
+
+        if data is None:
+            print("No extension named 'COADD' in spectrum fits file.")
+            return
+
+        spec_data = dict(
+            name=gal_info["name"],
+            wave=10 ** data["loglam"],
+            flux=data["flux"],
+            ivar=data["ivar"],
+        )
+
+        return spec_data
+
+    @staticmethod
+    def _parse_measurement(measurement):
+        meas_dict = {}
+
+        for k, v in measurement.items():
+            if not (k.endswith("_value") or k == "galaxy"):
+                continue
+
+            meas_dict[k.replace("_value", "")] = v
+
+            if k != "galaxy":
+                continue
+
+            gal_dict = {}
+
+            for gk, gv in v.items():
+                if gk in ["galaxy_id", "id"]:
+                    gk = "id"
+                    gv = str(gv)
+
+                gal_dict[gk] = gv
+
+            gal_dict["spectrum"] = DatabaseAPI._load_spectrum_data(gal_dict)
+
+            meas_dict[k] = gal_dict
+
+        return meas_dict
+
+    @staticmethod
+    def get_measurements(samples=False):
+        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/{'sample-' if samples else ''}measurements/{GLOBAL_STATE.student.id.value}"
         r = GLOBAL_STATE.request_session.get(url)
         res_json = r.json()
 
         measurements = []
 
         for measurement in res_json["measurements"]:
-            meas_dict = {}
-
-            for k, v in measurement.items():
-                if not (k.endswith("_value") or k == "galaxy"):
-                    continue
-
-                meas_dict[k.replace("_value", "")] = v
-
-                if k != "galaxy":
-                    continue
-
-                gal_dict = {}
-
-                for gk, gv in v.items():
-                    if gk in ["galaxy_id", "id"]:
-                        gk = "id"
-                        gv = str(gv)
-
-                    gal_dict[gk] = gv
-
-                # TODO: This is duplicated with the `_load_spectrum` from the
-                #  component state -- revisit and unify!
-                gal_type = gal_dict["type"]
-                gal_name = gal_dict["name"]
-                folder = {"Sp": "spiral", "E": "elliptical", "Ir": "irregular"}.get(
-                    gal_type
-                )
-
-                response = GLOBAL_STATE.request_session.get(
-                    f"{API_URL}/{HUBBLE_ROUTE_PATH}/spectra/{folder}/{gal_name}"
-                )
-
-                with closing(BytesIO(response.content)) as f:
-                    f.name = gal_name
-
-                    with fits.open(f) as hdulist:
-                        data = hdulist["COADD"].data if "COADD" in hdulist else None
-
-                if data is None:
-                    print("No extension named 'COADD' in spectrum fits file.")
-                    return
-
-                spec_data = dict(
-                    name=gal_name,
-                    wave=10 ** data["loglam"],
-                    flux=data["flux"],
-                    ivar=data["ivar"],
-                )
-
-                gal_dict["spectrum"] = spec_data
-
-                meas_dict[k] = gal_dict
+            meas_dict = DatabaseAPI._parse_measurement(measurement)
 
             measurement = StudentMeasurement(**meas_dict)
             measurements.append(measurement)
 
-            # NOTE: FOR DELETING DB MEASUREMENTS
-            # url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/measurement/{GLOBAL_STATE.student.id.value}/{measurement.galaxy.id}"
-            # r = GLOBAL_STATE.request_session.delete(url)
-            # print(r)
-
         return measurements
 
     @staticmethod
-    def put_measurements():
-        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/submit-measurement/"
+    def put_measurements(samples=False):
+        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/{'sample' if samples else 'submit'}-measurement/"
+        data = example_data if samples else student_data
 
-        for measurement in student_data.measurements:
+        for measurement in data.measurements:
             sub_dict = {
                 "student_id": GLOBAL_STATE.student.id.value,
                 "galaxy_id": int(measurement.galaxy.id),
@@ -116,15 +125,65 @@ class DatabaseAPI:
             r = GLOBAL_STATE.request_session.put(url, json=sub_dict)
 
     @staticmethod
+    def get_measurement(galaxy_id, samples=False):
+        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/{'sample-' if samples else ''}measurements/{GLOBAL_STATE.student.id.value}/{galaxy_id}"
+        r = GLOBAL_STATE.request_session.get(url)
+        res_json = r.json()
+
+        meas_dict = DatabaseAPI._parse_measurement(res_json["measurement"])
+
+        measurement = StudentMeasurement(**meas_dict)
+
+        return measurement
+
+    @staticmethod
+    def delete_all_measurements(samples=False):
+        url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/{'sample-' if samples else ''}measurements/{GLOBAL_STATE.student.id.value}"
+        r = GLOBAL_STATE.request_session.get(url)
+        res_json = r.json()
+
+        for measurement in res_json["measurements"]:
+            # NOTE: FOR DELETING DB MEASUREMENTS
+            url = f"{API_URL}/{HUBBLE_ROUTE_PATH}/{'sample-' if samples else ''}measurement/{GLOBAL_STATE.student.id.value}"
+            url = url + "/first" if samples else url + f"/{measurement['galaxy']['id']}"
+            r = GLOBAL_STATE.request_session.delete(url)
+            print("Deleted ", r)
+
+    @staticmethod
+    def get_sample_galaxy():
+        example_galaxy_data = GLOBAL_STATE.request_session.get(
+            f"{API_URL}/{HUBBLE_ROUTE_PATH}/sample-galaxy"
+        ).json()
+
+        example_galaxy_data = {k: example_galaxy_data[k] for k in example_galaxy_data}
+        example_galaxy_data["id"] = str(example_galaxy_data["id"])
+        example_galaxy_data["name"] = example_galaxy_data["name"].replace(".fits", "")
+
+        # Load the spectrum associated with the example data
+        spec_data = DatabaseAPI._load_spectrum_data(example_galaxy_data)
+        example_galaxy_data["spectrum"] = spec_data
+
+        return {
+            "rest_wave": round(ELEMENT_REST[example_galaxy_data["element"]]),
+            "galaxy": example_galaxy_data,
+        }
+
+    @staticmethod
     def get_story_state(component_state):
         r = GLOBAL_STATE.request_session.get(
             f"{API_URL}/story-state/{GLOBAL_STATE.student.id.value}/hubbles_law"
         )
         res_json = r.json()
 
-        app_state = res_json["state"]["app"]
-        story_state = res_json["state"]["story"]
-        stage_state = res_json["state"]["stage"][f"{component_state.stage_name.value}"]
+        try:
+            app_state = res_json["state"]["app"]
+            story_state = res_json["state"]["story"]
+            stage_state = res_json["state"]["stage"][
+                f"{component_state.stage_name.value}"
+            ]
+        except Exception as e:
+            print(f"Stored DB state is malformed; failed to load.\n{e}")
+            return
 
         # NOTE: the way the loading from a dict works, solara with trigger
         #  the reactive variables one after another, as there are loaded from
@@ -142,6 +201,7 @@ class DatabaseAPI:
 
     @staticmethod
     def put_story_state(component_state):
+        print("Serializing state into DB.")
         comp_state_dict = component_state.as_dict()
         comp_state_dict.update(
             {"current_step": component_state.current_step.value.value}
