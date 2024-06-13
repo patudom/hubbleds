7c5bf1bc21628a8fff94ac3bf4e8abb88563529e
Nicholas Earl <contact@nicholasearl.me>
2024-06-12 19:33:11 -0500

    Implement measurement data loading in top-level story start-up

--- src/hubbleds/layout.py
+++ src/hubbleds/layout.py
@@ -1,15 +1,28 @@
 from cosmicds.layout import BaseLayout
+from cosmicds.state import GLOBAL_STATE
 import solara
 from cosmicds.components import MathJaxSupport
+from .data_models.student import student_data, StudentMeasurement
+from .remote import DatabaseAPI
 
 
 @solara.component
 def Layout(children=[]):
 
-    # Mount external javascript libraries
-    def _load_math_jax():
-        MathJaxSupport()
+    with BaseLayout(
+        children=children, story_name="hubbles_law", story_title="Hubble's Law"
+    ):
+        # Mount external javascript libraries
+        def _load_math_jax():
+            MathJaxSupport()
 
-    solara.use_memo(_load_math_jax, dependencies=[])
+        solara.use_memo(_load_math_jax, dependencies=[])
 
-    BaseLayout(children=children, story_name="hubbles_law", story_title="Hubble's Law")
+        # Load student data measurements
+        def _load_student_measurements():
+            stored_measurements = DatabaseAPI.get_measurements()
+            student_data.measurements = stored_measurements
+
+        solara.use_memo(
+            _load_student_measurements, dependencies=[GLOBAL_STATE.student.id.value]
+        )
