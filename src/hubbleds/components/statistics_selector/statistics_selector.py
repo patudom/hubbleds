0344f307934d5b02ad0faa2003964bca8c36ee34
Carifio24 <carifio24@gmail.com>
2024-06-13 13:55:21 -0400

    Updates and simplification of statistics component.

--- src/hubbleds/components/statistics_selector/statistics_selector.py
+++ src/hubbleds/components/statistics_selector/statistics_selector.py
@@ -22,7 +22,7 @@ def find_statistic(stat, viewer, data, bins):
 def StatisticsSelector(viewers, glue_data, units, bins=None, statistics=["mean", "median", "mode"], **kwargs):
 
     transform = kwargs.get("transform", None)
-    color = kwargs.get("color", None)
+    color = kwargs.get("color", "#000000")
     line_ids = []
     bins = bins or [getattr(viewer.state, "bins", None) for viewer in viewers]
 
@@ -32,39 +32,45 @@ def StatisticsSelector(viewers, glue_data, units, bins=None, statistics=["mean",
         "median": "Description of the median",
     }
 
+    last_updated = None
+    selected = solara.use_reactive(None)
+
     def _remove_lines():
         for (viewer, viewer_line_ids) in zip(viewers, line_ids):
             lines = list(viewer.figure.select_traces(lambda t: t.meta in viewer_line_ids))
             viewer.figure.data = [t for t in viewer.figure.data if t not in lines]
             
+    def _update_lines():
+        stat = selected.value
+        if last_updated == stat:
+            return stat
 
-    def _update_lines(selected) -> List[str]:
-        _remove_lines()
+        if last_updated is not None:
+            _remove_lines()
 
-        if selected is None:
-            return []
+        if stat is None:
+            return None
 
-        line_ids = []
+        line_ids.clear()
         for viewer, d, bin, unit in zip(viewers, glue_data, bins, units):
             viewer_lines = []
             viewer_line_ids = []
-            for stat in selected:
-                try:
-                    capitalized = stat.capitalize()
-                    values = find_statistic(stat, viewer, d, bin)
-                    if transform is not None:
-                        values = [transform(v) for v in values]
-                    for value in values:
-                        label = f"{capitalized}: {value}"
-                        if unit:
-                            label += f" {unit}"
-                        line_id = str(uuid4())
-                        line = vertical_line_mark(viewer.layers[0], value, color, label=label)
-                        line["meta"] = line_id
-                        viewer_lines.append(line)
-                        viewer_line_ids.append(line_id)
-                except ValueError:
-                    pass
+            try:
+                capitalized = stat.capitalize()
+                values = find_statistic(stat, viewer, d, bin)
+                if transform is not None:
+                    values = [transform(v) for v in values]
+                for value in values:
+                    label = f"{capitalized}: {value}"
+                    if unit:
+                        label += f" {unit}"
+                    line_id = str(uuid4())
+                    line = vertical_line_mark(viewer.layers[0], value, color, label=label)
+                    line["meta"] = line_id
+                    viewer_lines.append(line)
+                    viewer_line_ids.append(line_id)
+            except ValueError:
+                pass
 
             # The Scatter traces that get added aren't the same instances
             # as those we pass in. So we need to grab references to them
@@ -72,29 +78,24 @@ def StatisticsSelector(viewers, glue_data, units, bins=None, statistics=["mean",
             viewer.figure.add_traces(viewer_lines)
             line_ids.append(viewer_line_ids)
 
-        return line_ids
+        return stat
 
 
-    selected = solara.use_reactive([])
     def _update_selected(stat, value):
-        if value and stat not in selected.value:
-            selected.set(selected.value + [stat])
-        else:
-          try:
-              index = selected.value.index(stat)
-              selected.set(selected.value[:index] + selected.value[index+1:])
-          except ValueError:
-              pass
+        nonlocal last_updated
+        if value:
+            selected.set(stat)
+        elif selected.value == stat:
+            selected.set(None)
+        last_updated = _update_lines()
 
-        new_ids = _update_lines(selected.value)
-        line_ids.clear()
-        line_ids.extend(new_ids)
-        
+    def _model_factory(stat):
+        return solara.lab.computed(lambda stat=stat: selected.value == stat)
 
     with rv.Card():
         with rv.Container():
             for stat in statistics:
-                model = solara.use_reactive(False)
+                model = _model_factory(stat)
                 # We need to bind the current value of `stat` to the lambda
                 solara.Switch(value=model,
                               label=stat.capitalize(),
