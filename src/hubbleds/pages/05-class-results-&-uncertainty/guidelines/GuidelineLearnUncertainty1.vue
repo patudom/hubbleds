02ddb24447fee2e23c54f6553da21eb600edff63
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-06-21 18:31:02 -0400

    Assign a class for this style that we use over and over

--- src/hubbleds/pages/05-class-results-&-uncertainty/guidelines/GuidelineLearnUncertainty1.vue
+++ src/hubbleds/pages/05-class-results-&-uncertainty/guidelines/GuidelineLearnUncertainty1.vue
@@ -8,7 +8,7 @@
   >
    
     <template #before-next>
-      Click <span style="background-color: #02838f; border-radius : 5px; padding: 3px; color:white!important;">UNCERTAINTY TUTORIAL</span> button.
+      Click <span class="inline-guideline-button tutorial">UNCERTAINTY TUTORIAL</span> button.
     </template>
 
     <div
@@ -18,7 +18,7 @@
         Before we go further, let's explore in more detail <b>why</b> different students may have obtained different age values for the universe.
       </p>
       <p>
-        Scientists explain this using a concept called <b>uncertainty</b>. Click the <span style="background-color: #02838f; border-radius : 5px; padding: 3px; color:white!important;">UNCERTAINTY TUTORIAL</span> button to learn about this important concept for interpreting data.
+        Scientists explain this using a concept called <b>uncertainty</b>. Click the <span class="inline-guideline-button tutorial">UNCERTAINTY TUTORIAL</span> button to learn about this important concept for interpreting data.
       </p>
     </div>
   </scaffold-alert>
