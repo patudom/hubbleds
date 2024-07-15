8733a928aecd649de85e4eb4383474d3ad497378
Carifio24 <carifio24@gmail.com>
2024-07-15 19:03:13 -0400

    Add layer toggle component for Plotly-based graph.

---
+++ src/hubbleds/components/plotly_layer_toggle/PlotlyLayerToggle.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card
+    flat
+    light
+    variant="outlined"
+    color="white"
+    class="layer_toggle"
+  >
+    <v-list-item-group
+      multiple
+      v-model="selected"
+    >
+      <v-list-item
+        v-for="(layer, index) in layer_indices"
+        :key="index"
+        :value="index"
+        inactive
+      >
+        <template v-slot:default="{ active }">
+          <v-list-item-content
+            class="font-weight-bold"
+          >
+            {{ labels[index] }}
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="index in selected"
+              :color="colors[index]"
+              @change="setLayerVisible(layer, active)"
+            />
+          </v-list-item-action>
+        </template>
+
+      </v-list-item>
+
+    </v-list-item-group>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["chart_id", "layer_indices", "labels", "colors", "initial_selected"],
+  data() {
+    return {
+      element: null,
+      selected: [],
+    };
+  },
+  async mounted() {
+    await window.plotlyPromise;
+    this.getElement();
+    this.selected = this.initial_selected || Array.from({length: this.layer_indices.length}, (x, i) => true);
+  },
+  methods: {
+    getElement() {
+      this.element = document.getElementById(this.chart_id);
+    },
+    setLayerVisible(index, visible) {
+      if (!this.element) {
+        this.getElement();
+      }
+      Plotly.update(this.element, { visible }, {}, index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-card {
+  border: solid 1px black !important;
+}
+</style>
---
+++ src/hubbleds/components/plotly_layer_toggle/plotly_layer_toggle.py
@@ -0,0 +1,11 @@
+import solara
+from typing import Iterable
+
+
+@solara.component_vue("PlotlyLayerToggle.vue")
+def PlotlyLayerToggle(chart_id: str,
+                      layer_indices: Iterable[int],
+                      labels: Iterable[str],
+                      colors: Iterable[str],
+):
+    pass
