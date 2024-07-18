db4500f7045c2b40a645faf839aa987820f2fbf4
patudom <patudom@users.noreply.github.com>
2024-07-18 17:16:26 -0400

    Update src/hubbleds/pages/02-distance-introduction/component_state.py
    Co-authored-by: Nicholas Earl <contact@nicholasearl.me>

--- src/hubbleds/pages/02-distance-introduction/component_state.py
+++ src/hubbleds/pages/02-distance-introduction/component_state.py
@@ -1,13 +1,14 @@
 import solara
 from pydantic import BaseModel
 from cosmicds.state import BaseState
+from hubbleds.base_component_state import BaseComponentState
 
 class DistanceSlideshow(BaseModel):
     step: int = 0
     max_step_completed: int = 0
     complete: bool = False
 
-class ComponentState(BaseState):
+class ComponentState(BaseComponentState, BaseState):
     stage_id: str = "distance_introduction"
     distance_slideshow_state: DistanceSlideshow = DistanceSlideshow()
 
