4f280032155a76f68510628cbc3d332c2407d919
Carifio24 <carifio24@gmail.com>
2024-07-23 16:19:33 -0400

    Pass local state into base layout.

--- src/hubbleds/layout.py
+++ src/hubbleds/layout.py
@@ -19,35 +19,31 @@ def Layout(children=[]):
     student_id = Ref(GLOBAL_STATE.fields.student.id)
     loaded_states = solara.use_reactive(False)
 
-    async def _load_measurements():
-  
-        # Load in the student's measurements
-        measurements = LOCAL_API.get_measurements(GLOBAL_STATE, LOCAL_STATE)
-        sample_measurements = LOCAL_API.get_sample_measurements(
-            GLOBAL_STATE, LOCAL_STATE
-        )
-
-        logger.info("Finished loading measurements.")
-        loaded_states.set(True)
-
-        Ref(LOCAL_STATE.fields.measurements_loaded).set(True)
-
-    def _on_student_info_loaded():
+    async def _load_global_local_states():
         if not GLOBAL_STATE.value.student.id:
-            logger.warning("Failed to load app and story states: no student was found.")
+            logger.warning("Failed to load measurements: no student was found.")
             return
 
         logger.info(
-            "Loading app and story states for user `%s`.",
+            "Loading story stage and measurements for user `%s`.",
             GLOBAL_STATE.value.student.id,
         )
 
         # Retrieve the student's app and local states
         LOCAL_API.get_app_story_states(GLOBAL_STATE, LOCAL_STATE)
 
-        logger.info("Finished loading state.")
+        # Load in the student's measurements
+        measurements = LOCAL_API.get_measurements(GLOBAL_STATE, LOCAL_STATE)
+        sample_measurements = LOCAL_API.get_sample_measurements(
+            GLOBAL_STATE, LOCAL_STATE
+        )
 
-        solara.lab.use_task(_load_measurements, dependencies=[])
+        logger.info("Finished loading state.")
+        loaded_states.set(True)
+
+        Ref(LOCAL_STATE.fields.measurements_loaded).set(True)
+
+    solara.lab.use_task(_load_global_local_states, dependencies=[student_id.value])
 
     # solara.use_memo(_load_local_state, dependencies=[student_id.value])
 
@@ -70,9 +66,9 @@ def Layout(children=[]):
     )
 
     with BaseLayout(
+        local_state=LOCAL_STATE,
         children=children,
         story_name=LOCAL_STATE.value.story_id,
         story_title=LOCAL_STATE.value.title,
-        on_student_info_loaded=_on_student_info_loaded,
     ):
         pass
