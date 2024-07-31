9db87b8388df981c9bf986966374cdb8dfddea27
John Arban Lewis <johnarban@users.noreply.github.com>
2024-07-31 10:46:53 -0400

    fix missing mc_callback on dot_seq8

--- src/hubbleds/pages/01-spectra-&-velocity/__init__.py
+++ src/hubbleds/pages/01-spectra-&-velocity/__init__.py
@@ -604,7 +604,12 @@ def Page():
                 event_next_callback=lambda _: transition_next(COMPONENT_STATE),
                 event_back_callback=lambda _: transition_previous(COMPONENT_STATE),
                 can_advance=COMPONENT_STATE.value.can_transition(next=True),
+                event_mc_callback=lambda event: mc_callback(event, LOCAL_STATE),
                 show=COMPONENT_STATE.value.is_current_step(Marker.dot_seq8),
+                state_view={
+                    "mc_score": get_multiple_choice(LOCAL_STATE, "vel_meas_consensus"),
+                    "score_tag": "vel_meas_consensus",
+                },
             )
             ScaffoldAlert(
                 GUIDELINE_ROOT / "GuidelineDotSequence09.vue",
