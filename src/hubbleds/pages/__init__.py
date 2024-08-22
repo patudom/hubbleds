725770ed86ae57bba9f452afbbb4b7e245fc5ea8
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-08-21 21:00:15 -0400

    Use vue version of intro slideshow instead of solara

--- src/hubbleds/pages/__init__.py
+++ src/hubbleds/pages/__init__.py
@@ -1,9 +1,36 @@
+
 import solara
-from ..components import IntroSlideshow
+from solara.toestand import Ref
+
+from hubbleds.components import IntroSlideshowVue
+from hubbleds.state import LOCAL_STATE, GLOBAL_STATE
+from .component_state import COMPONENT_STATE
+from hubbleds.remote import LOCAL_API
+from ..utils import IMAGE_BASE_URL
 
 from hubbleds.layout import Layout
+from cosmicds.logger import setup_logger
 
+logger = setup_logger("STAGE INTRO")
 
 @solara.component
 def Page():
-    IntroSlideshow()
+    router = solara.use_router()
+
+    IntroSlideshowVue(
+        step = COMPONENT_STATE.value.intro_slideshow_state.step,
+        length = 8,
+        titles = [
+        "Our Place in the Universe",
+        "Answering Questions with Data",
+        "Astronomy in the early 1900s",
+        "Explore the Cosmic Sky",
+        "What are the Fuzzy Things?",
+        "Spiral Nebulae and the Great Debate",
+        "Henrietta Leavitt's Discovery",
+        "Vesto Slipher and Spectral Data"
+        ],
+        image_location=f"{IMAGE_BASE_URL}/stage_intro",
+        event_slideshow_finished=lambda _: router.push("01-spectra-&-velocity"),
+        debug = LOCAL_STATE.value.debug_mode,
+    )
