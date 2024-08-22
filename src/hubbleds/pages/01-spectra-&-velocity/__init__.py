c64c6f97dbacf61e02d71011f2f5fbe9c8302a30
Carifio24 <carifio24@gmail.com>
2024-08-22 11:35:42 -0400

    Change fill data points to fill galaxies.

--- src/hubbleds/pages/01-spectra-&-velocity/__init__.py
+++ src/hubbleds/pages/01-spectra-&-velocity/__init__.py
@@ -212,11 +212,14 @@ def Page():
 
     solara.lab.use_task(_delay_selection_tool)
 
-    def _fill_data_points():
+    def _fill_galaxies():
         dummy_measurements = LOCAL_API.get_dummy_data()
+        measurements = []
         for measurement in dummy_measurements:
-            measurement.student_id = GLOBAL_STATE.value.student.id
-        Ref(LOCAL_STATE.fields.measurements).set(dummy_measurements)
+            measurements.append(StudentMeasurement(student_id=GLOBAL_STATE.value.student.id,
+                                                   galaxy=measurement.galaxy))
+        Ref(LOCAL_STATE.fields.measurements).set(measurements)
+
 
     def _select_random_galaxies():
         pass
@@ -307,7 +310,7 @@ def Page():
         with solara.Column():
             StateEditor(Marker, COMPONENT_STATE, LOCAL_STATE, LOCAL_API, show_all=False)
         with solara.Column():
-            solara.Button(label="Fill data points", on_click=_fill_data_points)
+            solara.Button(label="Fill galaxies", on_click=_fill_galaxies)
 
     with rv.Row():
         with rv.Col(cols=4):
