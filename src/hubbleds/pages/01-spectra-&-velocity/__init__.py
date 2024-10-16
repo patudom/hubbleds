069a4d77bace974ec66350d8584b65bbf8541593
John Arban Lewis <johnarban@users.noreply.github.com>
2024-10-16 16:05:02 -0400

    set show_example_data_table limit to dot_seq14

--- src/hubbleds/pages/01-spectra-&-velocity/__init__.py
+++ src/hubbleds/pages/01-spectra-&-velocity/__init__.py
@@ -577,7 +577,7 @@ def Page():
                 print("is galaxy selected:", galaxy_is_selected.value)             
 
             show_example_data_table = COMPONENT_STATE.value.current_step_between(
-            Marker.cho_row1, Marker.rem_gal1 #placeholder so it doesn't break - change to last new dot_seq marker.
+            Marker.cho_row1, Marker.dot_seq14 #placeholder so it doesn't break - change to last new dot_seq marker.
             )
             if show_example_data_table:
                 selection_tool_galaxy = selected_example_measurement
