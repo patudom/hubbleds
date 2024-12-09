a9e8e2dbc74ec6c1074ac1921517439f74df077e
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-12-09 18:36:22 -0500

    Update guideline text to say line instead of rectangle

--- src/hubbleds/pages/01-spectra-&-velocity/guidelines/GuidelineRestwave.vue
+++ src/hubbleds/pages/01-spectra-&-velocity/guidelines/GuidelineRestwave.vue
@@ -21,7 +21,7 @@
       <p
         v-if="!state_view.lambda_on"      
       >
-        The galaxy spectrum has {{ state_view.selected_example_galaxy.element == 'Mg-I' ? 'a' : 'an' }} {{ state_view.selected_example_galaxy.element }} {{ state_view.selected_example_galaxy.element == 'Mg-I' ? 'absorption' : 'emission' }} line marked by a red vertical rectangle on the graph.
+        The galaxy spectrum has {{ state_view.selected_example_galaxy.element == 'Mg-I' ? 'a' : 'an' }} {{ state_view.selected_example_galaxy.element }} {{ state_view.selected_example_galaxy.element == 'Mg-I' ? 'absorption' : 'emission' }} line marked by a red vertical line on the graph.
       </p>
       <p
         v-if="!state_view.lambda_used"
