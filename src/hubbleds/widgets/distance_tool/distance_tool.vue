74914d6b9ce55e38449c2685b63395e5d462e6ff
Carifio24 <carifio24@gmail.com>
2025-01-24 16:41:45 -0500

    Add buttons for refreshing tile cache to selection and distance tool widgets.

--- src/hubbleds/widgets/distance_tool/distance_tool.vue
+++ src/hubbleds/widgets/distance_tool/distance_tool.vue
@@ -81,6 +81,25 @@
         </template>
         {{ measuring ? 'Stop measuring' : 'Start measuring' }}
       </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            fab
+            dark
+            top 
+            right
+            absolute
+            style="margin-top: 96px"
+            color="var(--success-dark)"
+            class="selection-fab black--text"
+            v-bind="attrs"
+            v-on="on"
+            @click="clear_tile_cache()">
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+        </template>
+        Refresh images
+      </v-tooltip
     </div>
     <contrast-brightness-control 
       inline-style="padding: 0.5em 0;"
