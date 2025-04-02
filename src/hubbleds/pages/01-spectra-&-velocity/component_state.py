1ebb8f2798af6d4ed91846ef2490c1a2a538c5f8
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-04-01 23:29:21 -0400

    remove wwt ready gates

--- src/hubbleds/pages/01-spectra-&-velocity/component_state.py
+++ src/hubbleds/pages/01-spectra-&-velocity/component_state.py
@@ -139,14 +139,6 @@ class ComponentState(BaseComponentState, BaseState):
         )
 
     @property
-    def sel_gal1_gate(self) -> bool:
-        return self.wwt_ready
-
-    @property
-    def sel_gal2_gate(self) -> bool:
-        return self.wwt_ready
-
-    @property
     def not_gal1_gate(self) -> bool:
         return self.total_galaxies >= 1
 
