77ef05a6f0d669d550795b9679d55a4c6ff210c9
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-04-02 21:07:48 -0400

    Ensure base layout properties load before stage layout proprities; store only relevant routes (per 964)

--- src/hubbleds/layout.py
+++ src/hubbleds/layout.py
@@ -1,30 +1,43 @@
-from os import getenv
-from cosmicds.layout import BaseLayout
-from .state import GLOBAL_STATE, LOCAL_STATE
 import solara
-from solara.toestand import Ref
-from cosmicds.components import MathJaxSupport, PlotlySupport, GoogleAnalyticsSupport
-from hubbleds.remote import LOCAL_API
+from cosmicds.components import MathJaxSupport, PlotlySupport, \
+    GoogleAnalyticsSupport
+from cosmicds.layout import BaseLayout, BaseSetup
+from cosmicds.layout import BaseLayout, BaseSetup
 from cosmicds.logger import setup_logger
+from cosmicds.logger import setup_logger
+from hubbleds.remote import LOCAL_API
+from solara.toestand import Ref
+
+from .state import GLOBAL_STATE
+from .state import GLOBAL_STATE, LOCAL_STATE
 
 logger = setup_logger("LAYOUT")
 
 
 @solara.component
 def Layout(children=[]):
+    BaseSetup(
+        story_name=LOCAL_STATE.value.story_id,
+        story_title=LOCAL_STATE.value.title
+    )
 
     student_id = Ref(GLOBAL_STATE.fields.student.id)
     loaded_states = solara.use_reactive(False)
 
     router = solara.use_router()
     location = solara.use_context(solara.routing._location_context)
-    Ref(LOCAL_STATE.fields.last_route).set(router.path)
+    
+    route_current, routes_current_level = solara.use_route(peek=True)
+
+    if route_current in routes_current_level:
+        Ref(LOCAL_STATE.fields.last_route).set(router.path)
+
     route_index = next((i for i, r in enumerate(router.routes) if r.path == router.path.strip('/')), None)
     Ref(LOCAL_STATE.fields.max_route_index).set(max(route_index or 0, LOCAL_STATE.value.max_route_index or 0))
 
     def _load_global_local_states():
-        if not GLOBAL_STATE.value.student.id:
-            logger.warning("Failed to load measurements: no student was found.")
+        if student_id.value is None:
+            logger.warning(f"Failed to load measurements: ID `{GLOBAL_STATE.value.student.id}` not found.")
             return
 
         logger.info(
@@ -44,15 +57,13 @@ def Layout(children=[]):
         logger.info("Finished loading state.")
         if LOCAL_STATE.value.last_route is not None:
             router.push(LOCAL_STATE.value.last_route)
-        loaded_states.set(True)
 
         Ref(LOCAL_STATE.fields.measurements_loaded).set(True)
+        loaded_states.set(True)
 
-    # solara.lab.use_task(_load_global_local_states, dependencies=[student_id.value])
+    solara.use_memo(_load_global_local_states, dependencies=[])
 
-    solara.use_memo(_load_global_local_states, dependencies=[student_id.value])
-
-    async def _write_local_global_states():
+    def _write_local_global_states():
         if not loaded_states.value:
             return
 
@@ -67,17 +78,20 @@ def Layout(children=[]):
         if put_state and put_meas and put_samp:
             logger.info("Wrote state to database.")
         else:
-            logger.info(f"Did not write {'story state' if not put_state else ''} {'measurements' if not put_meas else ''} {'sample measurements' if not put_samp else ''} to database.")
+            logger.info(
+                f"Did not write {'story state' if not put_state else ''} "
+                f"{'measurements' if not put_meas else ''} "
+                f"{'sample measurements' if not put_samp else ''} "
+                f"to database.")
 
     solara.lab.use_task(
         _write_local_global_states, dependencies=[GLOBAL_STATE.value, LOCAL_STATE.value]
     )
 
-    with BaseLayout(
+    BaseLayout(
         local_state=LOCAL_STATE,
         children=children,
         story_name=LOCAL_STATE.value.story_id,
         story_title=LOCAL_STATE.value.title,
         force_demo=True
-    ):
-        pass
+    )
